2fc5a3668c735e2bd5a60bed36f9ec386d075435
adams <stilwelladam8@gmail.com>
2023-03-06 16:33:57 -0500

    Finished the Read4 code

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -10,9 +10,21 @@ public class ReadFour
      * Yes, you can look at the other 
      * examples in this lab to build your solution here.
      */
-        public static void main(String[] args)
-        {
+        public static void main(String[] args) {
             // Print out a running total of all the
             // values in the input file.
+            try {
+                Scanner fileIn = new Scanner(new File("input.txt"));
+                int sum = 0;
+                while (fileIn.hasNext()) {
+                    // Reads the entire line as a string
+                    String lineIn = fileIn.nextLine();
+                    System.out.println(lineIn);
+                    sum += Integer.parseInt(lineIn);
+                }
+                System.out.println("The Sum is: " + sum);
+            } catch (IOException e) {
+                System.out.println("File not found");
+            }
         }
 }
